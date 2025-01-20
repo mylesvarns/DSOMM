cc95c922f2294363e1b177a1e9ef8a5c2159a041
Myles Varns <myles.varns@mainline.com>
2025-01-20 15:39:33 -0600

    Removed github fork message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,13 +26,4 @@ describe('AppComponent', () => {
   it(`should have as title 'DSOMM'`, () => {
     expect(app.title).toEqual('DSOMM');
   });
-
-  it('check for fork me on github ribbon generation', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const HTMLElement: HTMLElement = fixture.nativeElement;
-    var divTag = HTMLElement.querySelector('div')!;
-    var aTag = divTag.querySelector('a')!;
-    //console.log(aTag);
-    expect(aTag.textContent).toEqual('Fork me on GitHub');
-  });
 });
